b140746c309b6fc0e91f8187b48efa7aa41736fc
Devin Beliveau <devinmarieb@gmail.com>
2016-09-06 14:16:15 -0600

    Add random button functionality and clean css

--- scripts.js
+++ scripts.js
@@ -2,6 +2,15 @@ function resetToWhite() {
   document.body.style.backgroundColor = "#FFFFFF";
 };
 
+function getRandomColor() {
+  var generateColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = generateColor;
+};
+
 $('#reset').on('click', function() {
   resetToWhite();
+});
+
+$('#random').on('click', function() {
+  getRandomColor();
 });
